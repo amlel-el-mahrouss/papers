7c91f13558e2de3ddab21ba234afd911e47dad01
Amlal El Mahrouss <amlal@nekernel.org>
2026-07-23 19:09:55 +0200

    [CHORE] src: updated install script.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+## To be used for curl -fsSL https://install.amlal.nekernel.org | sh
+
+echo "==> NE.APP: DOWNLOADING PUBLICATIONS..."
+
+ORG=publications-org
+PROFILE=pub-release
+
+SRC=pub
+VCS=git
+FLAGS=clone --recurse-submodules -j8
+DIST=src-${ORG}-${PROFILE}
+
+mkdir ${DIST}
+
+${GIT} ${FLAGS} https://github.com/publications-org/${SRC}.git ${DIST}/${SRC}
